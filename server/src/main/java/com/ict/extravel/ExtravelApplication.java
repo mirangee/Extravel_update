9c0665825be7391714caa1889f9c5c81ee156a70
sangjin <enrwls123@gmail.com>
2024-06-11 16:26:46 +0900

    JPA 연동

--- server/src/main/java/com/ict/extravel/ExtravelApplication.java
+++ server/src/main/java/com/ict/extravel/ExtravelApplication.java
@@ -1,6 +1,8 @@
 package com.ict.extravel;
 
+import com.ict.extravel.domain.currency.repository.CurrencyRepository;
 import com.ict.extravel.global.scheduler.exchage.ExchageCrolling;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +12,7 @@ public class ExtravelApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExtravelApplication.class, args);
         ExchageCrolling crolling = new ExchageCrolling();
-        crolling.getExchageData();
+        crolling.calculateExchageData();
     }
 
 }
--- server/src/main/java/com/ict/extravel/global/scheduler/exchage/ExchageCrolling.java
+++ server/src/main/java/com/ict/extravel/global/scheduler/exchage/ExchageCrolling.java
@@ -1,31 +1,76 @@
 package com.ict.extravel.global.scheduler.exchage;
 
-import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ExchageCrolling {
     private String exchangeApiKey = "QXYzwuIvJpiVoV1sNdTOBttWH6FdcRdh";
 
-    public void getExchageData() {
-        System.out.println(exchangeApiKey);
+    public void calculateExchageData(){
+        JSONArray exchageData = getExchageData("20230601");
+        Map<String,Double> weeks = new HashMap<String,Double>();
+        Map<String,Double> months = new HashMap<String,Double>();
+        exchageData.forEach(exchage -> {
+            JSONObject ob = (JSONObject)exchage;
+            weeks.put(ob.get("cur_unit").toString(),0.0);
+            months.put(ob.get("cur_unit").toString(),0.0);
+        });
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate from = LocalDate.parse("20230501",formatter);
+        LocalDate now = LocalDate.now();
+        long between = ChronoUnit.DAYS.between(from, now);
+        long between2 = ChronoUnit.WEEKS.between(from, now);
+        long between3 = ChronoUnit.MONTHS.between(from, now);
+        System.out.println(between);
+        System.out.println(between2);
+        System.out.println(between3);
+        LocalDate cur = from;
+        LocalDate preweek = from;
+        LocalDate premonth = from;
+        int weekcnt=0;
+        int monthcnt=0;
+        for(int i=0;i<between;i++){
+            cur = cur.plusDays(1);
+
+            if(ChronoUnit.WEEKS.between(preweek, cur) == 1){
+                preweek = cur;
+                weekcnt++;
+            }
+            if(ChronoUnit.MONTHS.between(premonth, cur) == 1){
+                premonth = cur;
+                monthcnt++;
+            }
+        }
+        System.out.println(weekcnt);
+        System.out.println(monthcnt);
+        System.out.println(cur);
+
+
+
+
+
+    }
+
+    private JSONArray getExchageData(String date) {
         String requestUrl = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON";
         URI uri = UriComponentsBuilder
                 .fromUriString(requestUrl)
                 .queryParam("authkey", exchangeApiKey)
-                .queryParam("searchdate", "20240604")
+                .queryParam("searchdate", date)
                 .queryParam("data", "AP01")
                 .encode()
                 .build()
@@ -35,7 +80,7 @@ public class ExchageCrolling {
         JSONParser parser = new JSONParser();
         try {
             JSONArray jsonObject = (JSONArray) parser.parse(forEntity.getBody());
-            System.out.println(jsonObject);
+            return jsonObject;
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
