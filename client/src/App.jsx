3785074077d6b1f7ba15d8d264019cf3ee54e4cb
xogns1101 <cocoseasky@naver.com>
2024-06-11 16:00:54 +0900

    헤더 간단하게 만듬 수정 필요

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,19 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
+// import ExchangeRateNews from './components/Slider';
+import Slider from './components/Slider';
+
 const App = () => {
-  return <div>First</div>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Slider />
+      </Router>
+    </>
+  );
 };
 
 export default App;
