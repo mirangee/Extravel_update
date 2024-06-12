436465ebfcfe5432cfde0982d6ee7f52aa8f6605
sangjin <enrwls123@gmail.com>
2024-06-12 16:01:40 +0900

    과거 환율데이터 크롤링 코드 작성

---
+++ server/src/main/java/com/ict/extravel/domain/member/controller/MemberController.java
@@ -0,0 +1,12 @@
+package com.ict.extravel.domain.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/auth")
+@RequiredArgsConstructor
+public class MemberController {
+
+}
