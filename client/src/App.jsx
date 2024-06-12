8467a5cdec4e0851035984c10d00bf90ed2b3759
mirangee <pmrang@live.co.kr>
2024-06-12 15:29:54 +0900

    Footer 구현 완료

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Section3 from './components/main/intro/Section3';
-import Login from './components/user/Login';
 
 const App = () => {
   return (
     <>
-      <Section1 />
       <Router>
         <Header />
       </Router>
-        <Section3 />
-      {/* <Footer /> */}
+      <Section1 />
+      <Section3 />
+      <Footer />
     </>
   );
 };
