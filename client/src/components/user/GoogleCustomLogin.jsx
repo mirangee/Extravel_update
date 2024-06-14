b006d91ff063663618312a6db6c9fcc88ae3b395
mirangee <pmrang@live.co.kr>
2024-06-14 16:31:20 +0900

    구글 소셜 로그인 회원 정보 가져오기 완료

--- client/src/components/user/GoogleCustomLogin.jsx
+++ client/src/components/user/GoogleCustomLogin.jsx
@@ -4,28 +4,34 @@ import googleCircle from '../../assets/img/google_circle.png';
 import axios from 'axios';
 
 const GoogleCustomLogin = () => {
-  const requestHandler = (tokenResponse) => {
-    const accessToken = tokenResponse.access_token;
-    axios
-      .post('http://localhost:8181/user/auth/google', {
-        accessToken,
-      })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .then(() => {
-        console.log('요청 시도 완료');
-      });
-  };
-
   const login = useGoogleLogin({
-    onSuccess: (tokenResponse) =>
-      requestHandler(tokenResponse),
-    onError: (error) => console.log(error),
+    onSuccess: async (tokenResponse) => {
+      try {
+        // Google API를 사용하여 사용자 정보를 가져옵니다.
+        const res = await axios.get(
+          'https://www.googleapis.com/oauth2/v3/userinfo',
+          {
+            headers: {
+              Authorization: `Bearer ${tokenResponse.access_token}`,
+            },
+          },
+        );
+        console.log(res.data); // 사용자 정보가 res.data에 포함됩니다.
+        const { email, name } = res.data;
+        console.log(`Email: ${email}, Name: ${name}`);
+        axios.post(
+          'http://localhost:8181/user/auth/google',
+          { name, email },
+        );
+      } catch (error) {
+        console.error('Error fetching user info:', error);
+      }
+    },
+    onError: (error) => {
+      console.error('Login Failed:', error);
+    },
   });
+
   return (
     <>
       <div
--- server/src/main/java/com/ict/extravel/domain/member/controller/MemberController.java
+++ server/src/main/java/com/ict/extravel/domain/member/controller/MemberController.java
@@ -1,14 +1,10 @@
 package com.ict.extravel.domain.member.controller;
-
-
-import com.ict.extravel.domain.member.dto.request.GoogleSignupRequestDTO;
+import com.ict.extravel.domain.member.dto.GoogleUserInfoDTO;
+import com.ict.extravel.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,26 +14,30 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Slf4j
 public class MemberController {
+    private final MemberService memberService;
 
     @Value("{spring.security.oauth2.client.registration.google.client-id}")
-    private String googleClientId;
+    private String clientId;
+
+    @Value("{spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
+
+    @Value("{spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String redirectUri;
+
+
+//    @PostMapping("/google/login")
+//    public ResponseEntity<?> googleLogin(@RequestBody String code) {
+//        log.info("google backend 들어옴!");
+//        memberService.getGoogleAccessToken(code, clientId, clientSecret, redirectUri);
+//        return ResponseEntity.ok().body("SUCCESS");
+//    }
 
     @PostMapping("/google")
-    public ResponseEntity<?> googleLogin(@Validated @RequestBody GoogleSignupRequestDTO requestDTO,
-                                         BindingResult result) {
-        log.info("google backend 들어옴!");
-        log.info(requestDTO.toString());
-        ResponseEntity<FieldError> resultEntity = getFieldErrorResponseEntity(result);
+    public ResponseEntity<?> googleLogin(@RequestBody GoogleUserInfoDTO googleUserInfoDTO) {
+        log.info(googleUserInfoDTO.getName());
+        log.info(googleUserInfoDTO.getEmail());
         return ResponseEntity.ok().body("SUCCESS");
-    }
-
-    private static ResponseEntity<FieldError> getFieldErrorResponseEntity(BindingResult result) {
-        if (result.hasErrors()) {
-            log.warn(result.toString());
-            return ResponseEntity.badRequest()
-                    .body(result.getFieldError());
-        }
-        return null;
     }
 }
 
