331517356beb2c82615999eeb3aec70644cd0451
mirangee <pmrang@live.co.kr>
2024-06-18 16:58:43 +0900

    payment 구현 중

--- client/src/components/main/intro/MainIntro.jsx
+++ client/src/components/main/intro/MainIntro.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Section1 from './Section1';
 import Section2 from './Section2';
 import Section3 from './Section3';
+import Payment from '../../Payment';
 
 const MainIntro = () => {
   return (
@@ -9,6 +10,7 @@ const MainIntro = () => {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Payment />
     </>
   );
 };
