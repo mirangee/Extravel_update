c6b3c736bfbbfc7fa52fc7cf86fc30348a3e384f
sangjin <enrwls123@gmail.com>
2024-06-27 10:24:15 +0900

    navershopping 수정

--- server/src/main/java/com/ict/extravel/domain/nation/controller/NaverShoppingController.java
+++ server/src/main/java/com/ict/extravel/domain/nation/controller/NaverShoppingController.java
@@ -32,7 +32,7 @@ public class NaverShoppingController {
         String Url = "https://openapi.naver.com/v1/search/shop.json";
 
         URI uri = UriComponentsBuilder.fromUriString(Url)
-                .queryParam("query", "일본 여행")
+                .queryParam("query", "아프리카 여행")
                 .queryParam("display" , 100)
                 .queryParam("filter", "naverpay")
                 .queryParam("start",1)
