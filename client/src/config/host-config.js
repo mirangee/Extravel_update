056f0d5adeb9710a86a84a3c240cb255c4be98a2
mirangee <pmrang@live.co.kr>
2024-07-10 18:19:01 +0900

    파비콘 적용

--- client/src/config/host-config.js
+++ client/src/config/host-config.js
@@ -3,10 +3,10 @@ const clientHostName = window.location.hostname;
 let backEndHostName;
 
 if (clientHostName === 'localhost') {
-  // backEndHostName = 'http://15.165.97.64'; // EC2 서버로 연결
-  backEndHostName = 'http://localhost:8181';
-} else if (clientHostName === 'spring.com') {
-  backEndHostName = 'https://api.spring.com';
+  backEndHostName = 'http://15.165.97.64'; // EC2 서버로 연결
+  // backEndHostName = 'http://localhost:8181';
+} else if (clientHostName === 'extravel.store') {
+  backEndHostName = 'http://15.165.97.64';
 }
 
 export const API_BASE_URL = backEndHostName;
